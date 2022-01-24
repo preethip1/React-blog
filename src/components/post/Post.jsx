41cc01671f024417b39bba3ef2f4ac184e0a9e67
Preethi <preethiknot1998@gmail.com>
2022-01-24 18:01:13 +0530

    single post,header,post,sidebar component

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./post.css";
+
+export default function Post() {
+  return (
+    <div className="post">
+      <img
+        className="postImg"
+        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">Music</span>
+          <span className="postCat">Life</span>
+        </div>
+        <span className="postTitle">Lorem ipsum dolor sit amet</span>
+        <br />
+        <span className="postDate">1 hour ago</span>
+      </div>
+      <p className="postDesc">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis,
+        possimus! Officia, soluta ad deleniti accusamus hic tempora cupiditate
+        placeat, voluptates autem dolores, est quidem sequi praesentium porro
+        debitis corporis. In perspiciatis fugit ducimus ipsam, sit debitis
+        impedit quae veniam ipsa earum a laborum voluptatem quia qui excepturi
+        amet, expedita aspernatur! Aspernatur, itaque. Inventore dolores quo
+        similique aliquid, doloremque neque, commodi quaerat magnam consequuntur
+        dolorum odit explicabo recusandae aliquam nulla. Non magni iure
+        deserunt? Velit, laudantium quam dignissimos officia itaque quibusdam
+        nulla ullam aperiam voluptatem earum necessitatibus, aspernatur
+        consequuntur optio, tempora natus. Veritatis, corrupti. A odit corrupti,
+        suscipit eveniet accusantium inventore aut necessitatibus excepturi!
+        Perspiciatis, labore dolor nam voluptates ratione non explicabo sapiente
+        excepturi cumque vel corporis quae vero esse rerum optio nemo voluptas.
+        Distinctio, enim. Cum cumque veritatis quidem tempore ratione. Totam
+        error eum voluptatibus nostrum. Tempora reiciendis facere deserunt ullam
+        dolores quo ducimus a placeat cumque molestiae suscipit sit tempore
+        harum officia doloribus corrupti assumenda laboriosam ut maxime
+        possimus, itaque nesciunt. Aut atque fuga, in doloribus qui quam quidem
+        veritatis quod facilis eligendi alias delectus quaerat saepe veniam
+        minus voluptas! Quas illo tempora inventore maxime nam deleniti itaque
+        odio quaerat ipsum eum vitae, repudiandae quisquam, ad perferendis
+        reprehenderit vero recusandae sint eaque hic neque ipsa! Itaque autem,
+        dolores dolorem recusandae illo vero repellat enim saepe necessitatibus
+        ullam unde, vel ratione ab doloremque! Quae, recusandae quos, tenetur
+        laborum corporis blanditiis reiciendis delectus dolores dolore sed
+        aspernatur possimus? Magni omnis nulla dicta molestiae facere neque
+        totam maxime non beatae. Tempore, necessitatibus.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './singlePost.css';
+
+export default function SinglePost() {
+    return ( 
+        <div className='singlePost'>
+            singlePost
+        </div>
+     );
+}
+
